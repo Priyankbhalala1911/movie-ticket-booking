b0912ec58c75740ed089fba045d944781d7b2cd9
priya <priyankbhalala1911@gmail.com>
2025-01-29 15:54:21 +0530

    add movie and movie advertisment scrolling

--- HomePage/home.js
+++ HomePage/home.js
@@ -1,19 +1,67 @@
-let index = 0;
+let counter = 0;
+let counter1 = 0;
 
-function showMovie() {
-    const frame = document.querySelector(".movie_list");
-    const totalFrame = document.querySelectorAll(".movie_list .movie_container").length;
-    if (index >= totalFrame) index = 0;
-    if (index < 0) index = totalFrame - 1;
-    frame.style.transform = `translateX(${-index * 55}%)`;
+autoScrolling();
+const slides = document.querySelectorAll(".movie_list .movie_container");
+
+const next = document.querySelector(".right_side");
+const prev = document.querySelector(".left_side");
+
+next.addEventListener("click", slideNext);
+prev.addEventListener("click", slidePrev);
+
+function slideNext() {
+    counter = (counter + 1) % slides.length;
+    updateSlidePosition();
 }
 
-function newMovie() {
-    index++;
-    showMovie();
+function slidePrev() {
+    counter = (counter - 1 + slides.length) % slides.length;
+    updateSlidePosition();
 }
 
-function prevMovie() {
-    index--;
-    showMovie();
+function updateSlidePosition() {
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(-${counter * 575}px)`;
+    });
+}
+
+
+const slideNews = document.querySelectorAll(".slide");
+const nextNews = document.querySelector(".nextnews");
+const prevNews = document.querySelector(".prevnews");
+const dots = document.querySelectorAll(".dot")
+
+nextNews.addEventListener("click", slideNewsNext);
+prevNews.addEventListener("click", slideNewsPrev);
+
+function slideNewsNext() {
+    counter1 = (counter1 + 1) % slideNews.length;
+    updateSlideNewsPosition();
+    indicators();
+}
+
+function slideNewsPrev() {
+    counter1 = (counter1 - 1 + slideNews.length) % slideNews.length;
+    updateSlideNewsPosition();
+    indicators();
+}
+
+function updateSlideNewsPosition() {
+    slideNews.forEach((slide, index) => {
+        slide.style.transform = `translateX(-${counter1 * 100}%)`;
+    });
+}
+
+function autoScrolling() {
+    setInterval(() => {
+        slideNewsNext();
+    }, 3000);
+}
+
+function indicators() {
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    dots[counter1].className += " active";
 }
